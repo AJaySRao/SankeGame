e7d22538779d4dfae54438ebcccac99b17261b68
changeAJay <ajajay008@gmail.com>
2022-02-25 21:00:27 +0530

    1. When snake eats an egg Increase the count of consumed egg to keep track of the score and increase size of snake. 2. GAME OVER if the snake hits the wall or bites itself/own tail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 from snake import Snake
 import time
+from food import Egg
+from score import ScoreBoard
 
 s = Screen()
 s.setup(600, 600)
@@ -8,6 +10,8 @@ s.bgcolor('black')
 s.tracer(0)
 
 snake = Snake()
+egg = Egg()
+score = ScoreBoard()
 
 s.listen()
 game = True
@@ -21,6 +25,37 @@ while game:
     s.update()
     time.sleep(0.1)
     snake.move()
+
+    #When snake eats a egg
+    #size of egg is 10/10.
+    #Size of snake body each is 20/20.
+    #So when the distance of 2 segments is less than 15 there is a chance snake eating the egg .
+    #if that happens move the egg to next random position.
+    #Increase the count of consumed egg to keep track of the score and increase size of snake.
+    if snake.snake_head.distance(egg) <= 15:
+        egg.move()
+        snake.extend_segment()
+        score.update_score()
+
+    #When snake hits his head to wall
+    #Size of screen is set to 600/600
+    #So x & y axis are -300 to 300 each
+    #Need some space over so that the body actually looks like touching the edges of the screen
+    #so cutting of 10/20 each side from 300
+    # if the snake head touches any given number then the game stops
+    if snake.snake_head.xcor() <= -290 or snake.snake_head.xcor() >= 290 or snake.snake_head.ycor() >= 280 or snake.\
+            snake_head.ycor() <= -290:
+        score.game_over()
+        game = False
+
+    #When the snake touches its own body
+    for snake_body in snake.segments:
+        if snake_body == snake.snake_head:
+            pass
+        elif snake.snake_head.distance(snake_body) < 10:
+            score.game_over()
+            game = False
+
 
 
 
