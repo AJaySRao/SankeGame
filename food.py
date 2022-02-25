b7978fd6a76d6e49c7cc67b6938741c743fced77
changeAJay <ajajay008@gmail.com>
2022-02-25 08:12:40 +0530

    Created a food class that inherits the turtle class, set size to 10/10, shape to circle and also moves to random position when snake head touches the food

---
+++ food.py
@@ -0,0 +1,18 @@
+import random
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color('blue')
+        self.move()
+
+    def move(self):
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 265)
+        self.goto(x, y)
