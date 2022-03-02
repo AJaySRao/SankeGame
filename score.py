a69acf0b22c91a9c1a3c559662face613fa88873
changeAJay <ajajay008@gmail.com>
2022-03-02 06:55:30 +0530

    Added high score that compares with old score when a snake hits wall or its tail

--- score.py
+++ score.py
@@ -10,14 +10,25 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.goto(0, 270)
         self.S = 0
+        self.HS = 0
         self.update_score()
 
     def update_score(self):
         self.clear()
-        self.write(f"Score : {self.S} ", align='center', font=('Courier', 18, 'normal'))
-        self.S += 1
+        self.write(f"Score : {self.S} High Score : {self.HS} ", align='center', font=('Courier', 18, 'normal'))
 
-    def game_over(self):
-        self.clear()
-        self.setposition(0, 0)
-        self.write(f"G A M E  O V E R", align='center', font=('Arial', 24, 'bold'))
+    def score_increase(self):
+        self.S += 1
+        self.update_score()
+
+    # def game_over(self):
+    #     self.clear()
+    #     self.setposition(0, 0)
+    #     self.write(f"G A M E  O V E R", align='center', font=('Arial', 24, 'bold'))
+    #     self.compare_score()
+
+    def compare_score(self):
+        if self.S > self.HS:
+            self.HS = self.S
+        self.S = 0
+        self.update_score()
